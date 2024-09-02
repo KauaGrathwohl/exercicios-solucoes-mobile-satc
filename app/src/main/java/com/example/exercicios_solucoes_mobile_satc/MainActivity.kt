f4603488c49b143573ab7b26775fea7f509a1a98
kauagrathwohl <kauagrathwohl095@gmail.com>
2024-09-02 18:10:31 -0300

    initial commit

---
+++ app/src/main/java/com/example/exercicios_solucoes_mobile_satc/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.exercicios_solucoes_mobile_satc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.exercicios_solucoes_mobile_satc.ui.theme.Exercicios_solucoes_mobile_satcTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Exercicios_solucoes_mobile_satcTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Exercicios_solucoes_mobile_satcTheme {
+        Greeting("Android")
+    }
+}
